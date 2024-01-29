6a54179982649003efe4c9a47d0823518bf6520f
Asa White <whiteasa94@gmail.com>
2024-01-29 01:16:13 -0800

    made base level

--- Player.gd
+++ Player.gd
@@ -1,14 +1,18 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("hello World")
-	print("Dont Panic")
-	print(42)
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		print("The spacebar is pressed")
+		position.y += delta
+
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
+
+	if Input.is_action_pressed("ui_up"):
+		position.y += delta
+
+	if Input.is_action_pressed("ui_down"):
+		position.y -= delta
