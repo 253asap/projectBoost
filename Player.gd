37b0a426b7b57a3da60e1371991e573082fed3ce
Asa White <whiteasa94@gmail.com>
2024-01-29 21:15:06 -0800

    added game loop and levels

--- Player.gd
+++ Player.gd
@@ -1,18 +1,31 @@
-extends Node3D
+extends RigidBody3D
+
+## How much vertical force to apply when moving
+@export_range(750.0, 3000.0) var thrust: float = 1000.0
+@export var torqueThrust: float = 100.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * thrust)
 
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+	if Input.is_action_pressed("rotateLeft"):
+		apply_torque(Vector3(0.0, 0.0, torqueThrust * delta))
 
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_action_pressed("rotateRight"):
+		apply_torque(Vector3(0.0, 0.0, -torqueThrust * delta))
 
-	if Input.is_action_pressed("ui_up"):
-		position.y += delta
 
-	if Input.is_action_pressed("ui_down"):
-		position.y -= delta
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		completeLevel(body.file_path)
+	if "Hazard" in body.get_groups():
+		crashSequence()
+		
+func crashSequence() -> void:
+	print("You lose")
+	get_tree().reload_current_scene()
+
+func completeLevel(next_level_file: String) -> void:
+	print("winner winner chicken dinner")
+	get_tree().change_scene_to_file(next_level_file)
