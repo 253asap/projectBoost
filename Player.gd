e827d62fa71f7c963a9d63fa324cb6b50e8b890b
Asa White <whiteasa94@gmail.com>
2024-01-31 19:55:10 -0800

    tweeeeens

--- Player.gd
+++ Player.gd
@@ -4,6 +4,8 @@ extends RigidBody3D
 @export_range(750.0, 3000.0) var thrust: float = 1000.0
 @export var torqueThrust: float = 100.0
 
+var isTransitioning := false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
@@ -17,15 +19,23 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node) -> void:
-	if "Goal" in body.get_groups():
-		completeLevel(body.file_path)
-	if "Hazard" in body.get_groups():
-		crashSequence()
+	if !isTransitioning:
+		if "Goal" in body.get_groups():
+			completeLevel(body.file_path)
+		if "Hazard" in body.get_groups():
+			crashSequence()
 		
 func crashSequence() -> void:
 	print("You lose")
-	get_tree().reload_current_scene()
+	tweenTrans(get_tree().reload_current_scene)
 
 func completeLevel(next_level_file: String) -> void:
 	print("winner winner chicken dinner")
-	get_tree().change_scene_to_file(next_level_file)
+	tweenTrans(get_tree().change_scene_to_file.bind(next_level_file))
+
+func tweenTrans(callFunc: Callable) -> void:
+	set_process(false)
+	isTransitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.5)
+	tween.tween_callback(callFunc)
